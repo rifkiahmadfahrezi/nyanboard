3d38e26c6e3f0516b11a5a11449fc3ecaf9d688d
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2025-02-20 21:50:14 +0700

    add navbar

---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,35 @@
+import { Button } from "@/components/ui/button";
+import { Logo } from "../logo";
+import { NavMenu } from "./nav-menu";
+import { NavigationSheet } from "./navigation-sheet";
+import { cn } from "@/lib/utils";
+
+export const Navbar = ({
+  className,
+  ...props
+}: React.ComponentProps<'header'>) => {
+  return (
+    <header className={cn('top-0 sticky z-50',className)} {...props}>
+      <nav className="h-16 bg-background border-b">
+        <div className="h-full flex items-center justify-between max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
+          <Logo />
+
+          {/* Desktop Menu */}
+          <NavMenu className="hidden md:block" />
+
+          <div className="flex items-center gap-3">
+            <Button variant="outline" className="hidden sm:inline-flex">
+              Sign In
+            </Button>
+            <Button>Get Started</Button>
+
+            {/* Mobile Menu */}
+            <div className="md:hidden">
+              <NavigationSheet />
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
---
+++ src/components/navbar/navigation-sheet.tsx
@@ -0,0 +1,21 @@
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Menu } from "lucide-react";
+import { NavMenu } from "./nav-menu";
+
+export const NavigationSheet = () => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent>
+        <div className="flex flex-col px-3 mt-10">
+          <NavMenu orientation="vertical" />
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+};
