bf650bcd48a76695add7e2289997ed85a2daa09f
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2025-02-20 21:50:09 +0700

    create footer

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,180 @@
+import { Separator } from "@/components/ui/separator";
+import {
+  DribbbleIcon,
+  GithubIcon,
+  TwitchIcon,
+  TwitterIcon,
+} from "lucide-react";
+import Link from "next/link";
+import { Logo } from "../logo";
+import { cn } from "@/lib/utils";
+
+const footerSections = [
+  {
+    title: "Product",
+    links: [
+      {
+        title: "Overview",
+        href: "#",
+      },
+      {
+        title: "Pricing",
+        href: "#",
+      },
+      {
+        title: "Releases",
+        href: "#",
+      },
+    ],
+  },
+  {
+    title: "Company",
+    links: [
+      {
+        title: "About us",
+        href: "#",
+      },
+      {
+        title: "Careers",
+        href: "#",
+      },
+      {
+        title: "Contact",
+        href: "#",
+      },
+    ],
+  },
+  {
+    title: "Resources",
+    links: [
+      {
+        title: "Blog",
+        href: "#",
+      },
+      {
+        title: "Newsletter",
+        href: "#",
+      },
+      {
+        title: "Events",
+        href: "#",
+      },
+      {
+        title: "Support",
+        href: "#",
+      },
+    ],
+  },
+  {
+    title: "Social",
+    links: [
+      {
+        title: "Twitter",
+        href: "#",
+      },
+      {
+        title: "LinkedIn",
+        href: "#",
+      },
+      {
+        title: "Facebook",
+        href: "#",
+      },
+      {
+        title: "GitHub",
+        href: "#",
+      },
+      {
+        title: "AngelList",
+        href: "#",
+      },
+      {
+        title: "Dribbble",
+        href: "#",
+      },
+    ],
+  },
+  {
+    title: "Legal",
+    links: [
+      {
+        title: "Terms",
+        href: "#",
+      },
+      {
+        title: "Privacy",
+        href: "#",
+      },
+      {
+        title: "Cookies",
+        href: "#",
+      },
+    ],
+  },
+];
+
+export const Footer = ({
+  className,
+  ...props
+}: React.ComponentProps<'footer'>) => {
+  return (
+      <footer className={cn("border-t px-2", className)} {...props}>
+        <div className="max-w-screen-xl mx-auto">
+          <div className="py-12 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-7 gap-x-8 gap-y-10 px-6 xl:px-0">
+            <div className="col-span-full xl:col-span-2">
+               <Logo />
+
+              <p className="mt-4 text-muted-foreground">
+                Design amazing digital experiences that create more happy in the
+                world.
+              </p>
+            </div>
+
+            {footerSections.map(({ title, links }) => (
+              <div key={title}>
+                <h6 className="font-semibold">{title}</h6>
+                <ul className="mt-6 space-y-4">
+                  {links.map(({ title, href }) => (
+                    <li key={title}>
+                      <Link
+                        href={href}
+                        className="text-muted-foreground hover:text-foreground"
+                      >
+                        {title}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </div>
+          <Separator />
+          <div className="py-8 flex flex-col-reverse sm:flex-row items-center justify-between gap-x-2 gap-y-5 px-6 xl:px-0">
+            {/* Copyright */}
+            <span className="text-muted-foreground">
+              &copy; {new Date().getFullYear()}{" "}
+              <Link href="/" target="_blank">
+                Shadcn UI Blocks
+              </Link>
+              . All rights reserved.
+            </span>
+
+            <div className="flex items-center gap-5 text-muted-foreground">
+              <Link href="#" target="_blank">
+                <TwitterIcon className="h-5 w-5" />
+              </Link>
+              <Link href="#" target="_blank">
+                <DribbbleIcon className="h-5 w-5" />
+              </Link>
+              <Link href="#" target="_blank">
+                <TwitchIcon className="h-5 w-5" />
+              </Link>
+              <Link href="#" target="_blank">
+                <GithubIcon className="h-5 w-5" />
+              </Link>
+            </div>
+          </div>
+        </div>
+      </footer>
+  );
+};
