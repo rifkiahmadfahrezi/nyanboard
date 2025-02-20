565ee2d46bf4bc3ee9785a192e9f2b8b0ba96220
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2025-02-20 21:51:09 +0700

    add layout and homepage components with updated global styles

---
+++ src/app/(app)/layout.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import { Navbar } from '@/components/navbar'
+import { Footer } from '@/components/footer'
+
+const Layout = ({
+   children
+}: {
+   children: React.ReactNode
+}) => {
+  return (
+   <>
+   <Navbar />
+   <main className="min-h-svh">
+   {children}
+   </main>
+   <Footer />
+   </>
+  )
+}
+
+export default Layout
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist } from "next/font/google";
 import "./globals.css";
+import Providers from "@/components/providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
   subsets: ["latin"],
 });
 
@@ -25,9 +21,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.className} antialiased`}
       >
+        <Providers>
         {children}
+        </Providers>
       </body>
     </html>
   );
