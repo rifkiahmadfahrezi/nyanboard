2fbc9cf03e2d2f582a73a209c24082bb0f8f38a6
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2025-02-21 15:03:26 +0700

    add Radix UI components: Avatar, Switch, Collapsible, and Breadcrumb

---
+++ src/components/ui/collapsible.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import { cn } from "@/lib/utils"
+import * as CollapsiblePrimitive from "@radix-ui/react-collapsible"
+
+function Collapsible({
+  ...props
+}: React.ComponentProps<typeof CollapsiblePrimitive.Root>) {
+  return <CollapsiblePrimitive.Root data-slot="collapsible" {...props} />
+}
+
+function CollapsibleTrigger({
+  ...props
+}: React.ComponentProps<typeof CollapsiblePrimitive.CollapsibleTrigger>) {
+  return (
+    <CollapsiblePrimitive.CollapsibleTrigger
+      data-slot="collapsible-trigger"
+      {...props}
+    />
+  )
+}
+
+function CollapsibleContent({
+  className,
+  ...props
+}: React.ComponentProps<typeof CollapsiblePrimitive.CollapsibleContent>) {
+  return (
+    <CollapsiblePrimitive.CollapsibleContent
+      data-slot="collapsible-content"
+      className={cn('CollapsibleContent',className)}
+      {...props}
+    />
+  )
+}
+
+export { Collapsible, CollapsibleTrigger, CollapsibleContent }
