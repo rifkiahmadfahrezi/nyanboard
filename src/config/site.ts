032206e65e8950798145c8c46baf9880c20a168b
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2025-02-21 15:04:24 +0700

    feat: add site configuration and update metadata in layout

---
+++ src/config/site.ts
@@ -0,0 +1,10 @@
+export const siteConfig = {
+   name: "Nyashboard",
+   description: "A free and open-source nextjs shadcn dashboard template",
+   ogImage: "/og.jpg",
+   links: {
+      github: 'https://github.com/rifkiahmadfahrezi',
+   }
+}
+
+export type SiteConfig = typeof siteConfig
