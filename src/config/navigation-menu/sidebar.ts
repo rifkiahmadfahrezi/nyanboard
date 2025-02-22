74a491e11abc6d309fec94def68218eddfbb1bcd
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2025-02-22 16:01:07 +0700

    refactor: update sidebar routes

--- src/config/navigation-menu/sidebar.ts
+++ src/config/navigation-menu/sidebar.ts
@@ -1,118 +1,44 @@
 import type { SidebarMenu } from "@/types/sidebar-menu";
 
-import { BookOpen, Bot, Frame, PieChart, Settings2, Map } from "lucide-react";
-import { SquareTerminal } from "lucide-react";
+import { House, LayoutDashboard, Layers } from "lucide-react";
 
 export const sidebarMenus: SidebarMenu[] = [
   {
-    title: "Platform",
     menus: [
       {
-        label: "Playground",
-        url: "#",
-        icon: SquareTerminal,
-        isActive: true,
-        items: [
-          {
-            label: "History",
-            url: "#",
-          },
-          {
-            label: "Starred",
-            url: "#",
-          },
-          {
-            label: "Settings",
-            url: "#",
-          },
-        ],
-      },
-      {
-        label: "Models",
-        url: "#",
-        icon: Bot,
-        items: [
-          {
-            label: "Genesis",
-            url: "#",
-          },
-          {
-            label: "Explorer",
-            url: "#",
-          },
-          {
-            label: "Quantum",
-            url: "#",
-          },
-        ],
-      },
-      {
-        label: "Documentation",
-        url: "#",
-        icon: BookOpen,
-        items: [
-          {
-            label: "Introduction",
-            url: "#",
-          },
-          {
-            label: "Get Started",
-            url: "#",
-          },
-          {
-            label: "Tutorials",
-            url: "#",
-          },
-          {
-            label: "Changelog",
-            url: "#",
-          },
-        ],
-      },
-      {
-        label: "Settings",
-        url: "#",
-        icon: Settings2,
-        items: [
-          {
-            label: "General",
-            url: "#",
-          },
-          {
-            label: "Team",
-            url: "#",
-          },
-          {
-            label: "Billing",
-            url: "#",
-          },
-          {
-            label: "Limits",
-            url: "#",
-          },
-        ],
+        label: "Dashboard",
+        url: "/dashboard",
+        icon: House,
       },
     ],
   },
 
-  // projects menus
+  // ui menus
   {
-    title: "Project",
+    title: "UI",
     menus: [
       {
-        label: "Design Engineering",
-        url: "#",
-        icon: Frame,
-      },
-      {
-        label: "Sales & Marketing",
-        url: "#",
-        icon: PieChart,
-      },
-      {
-        label: "Travel",
-        url: "#",
-        icon: Map,
+        label: "Components",
+        url: "/dashboard/components",
+        icon: LayoutDashboard,
+        items: [
+          {
+            label: "Editor",
+            url: "/dashboard/components/editor",
+          },
+          {
+            label: "Overview card",
+            url: "/dashboard/components/overview card",
+          },
+          {
+            label: "Charts",
+            url: "/dashboard/components/charts",
+          },
+          {
+            label: "Multistep form",
+            url: "/dashboard/components/multistep-form",
+          },
+        ],
       },
     ],
   },
