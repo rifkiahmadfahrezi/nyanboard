8695a9c3d3f4581a8ef670aadb821bdfbc742f21
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2025-02-23 09:43:48 +0700

    change navbar bg color

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -11,7 +11,7 @@ export const Navbar = ({
 }: React.ComponentProps<"header">) => {
   return (
     <header className={cn("top-0 sticky z-50", className)} {...props}>
-      <nav className="h-16 bg-background border-b">
+      <nav className="h-16 bg-background/30 backdrop-blur-sm">
         <div className="h-full flex items-center justify-between max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
           <Logo />
 
@@ -19,7 +19,7 @@ export const Navbar = ({
           <NavMenu className="hidden md:block" />
 
           <div className="flex items-center gap-3">
-            <Button asChild variant="outline" className="hidden sm:inline-flex">
+            <Button asChild variant="ghost" className="hidden sm:inline-flex">
               <Link href={"/sign-in"}>Sign In</Link>
             </Button>
             <Button asChild>
