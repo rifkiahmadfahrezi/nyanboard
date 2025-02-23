cacc72316294c76442c9a37e4cc5e152c5849bce
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2025-02-23 09:44:11 +0700

    feat: create tanstack and progress bar provider

--- src/components/providers/index.tsx
+++ src/components/providers/index.tsx
@@ -2,6 +2,8 @@
 
 import React from "react";
 import { ThemeProvider } from "./theme-provider";
+import { TanstackProvider } from "./tanstack-provider";
+import { ProgressbarProvider } from "./progressbar-provider";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -12,7 +14,9 @@ const Providers = ({ children }: { children: React.ReactNode }) => {
         enableSystem
         disableTransitionOnChange
       >
-        {children}
+        <ProgressbarProvider>
+          <TanstackProvider>{children}</TanstackProvider>
+        </ProgressbarProvider>
       </ThemeProvider>
     </>
   );
---
+++ src/components/providers/progressbar-provider.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import React from "react";
+import { AppProgressBar as ProgressBar } from "next-nprogress-bar";
+import { siteConfig } from "@/config/site";
+
+const { color, height, spinner } = siteConfig.progressbar;
+export const ProgressbarProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  return (
+    <>
+      {children}
+      <ProgressBar
+        height={height}
+        color={color}
+        options={{ showSpinner: spinner }}
+        shallowRouting
+      />
+    </>
+  );
+};
